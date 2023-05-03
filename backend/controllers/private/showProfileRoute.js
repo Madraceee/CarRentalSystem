c6cf2a125ca77878dfb181e61c6abf44512e4bc9
madraceee <raajanitheesh@gmail.com>
2023-05-03 16:51:21 +0530

    Updated showProfile

--- backend/controllers/private/showProfileRoute.js
+++ backend/controllers/private/showProfileRoute.js
@@ -1,29 +1,38 @@
 import ProfileSchema from "../../database/model/profileSchema.js";
 import Profile from "../../database/repository/profile.js";
+import auth from "./auth.js";
 
 async function displayRoute(data, res) {
   // Parse request data and create profile object
-  const jsonData = JSON.parse(data.data);
-  const profileObj = ProfileSchema.create(jsonData);
+  const authStatus = await auth(data,res);
+  if(authStatus.auth === true){
+    
+    try {
+      // Attempt to fetch profile details
+      const jsonData = JSON.parse(data.data); 
+      const profileObj = ProfileSchema.create(jsonData)
+      const payload = await Profile.profile(profileObj);
 
-  try {
-    // Attempt to fetch profile details
-    const payload = await Profile.profile(profileObj);
-
-    // Send response with payload
-    let payloadStr = JSON.stringify(payload);
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    if (payload.msg === "Profile Not Found") {
-      res.writeHead(404);
-    } else {
-      res.writeHead(200);
+      //Do not send password for showProfile
+      if(payload.msg === "Profile Listing Details Found"){
+        payload.profileDetails.password = "";
+      }      
+  
+      // Send response with payload
+      let payloadStr = JSON.stringify(payload);
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      if (payload.msg === "Profile Not Found") {
+        res.writeHead(404);
+      } else {
+        res.writeHead(200);
+      }
+      res.write(payloadStr);
+      res.end("\n");
+    } catch (err) {
+      console.log("Display Route Error:" + err);
     }
-    res.write(payloadStr);
-    res.end("\n");
-  } catch (err) {
-    console.log("Display Route Error:" + err);
-  }
+  }  
 }
 
-export { displayRoute };
+export default displayRoute;
--- backend/database/model/profileSchema.js
+++ backend/database/model/profileSchema.js
@@ -1,15 +1,17 @@
 class ProfileSchema {
-    constructor(emailId, password, city, address, role) {
+    constructor(emailId,name,password, city, address, role) {
       this.emailId = emailId;
-      this.password = password;
-      this.city = city;
-      this.address = address;
-      this.role = role;
+      this.name = name;
+      this.password = password ? password : null;
+      this.city = city? city : null;
+      this.address = address? address : null;
+      this.role = role? role : null;
     }
   
     static create(data) {
       return new ProfileSchema(
         data.emailId,
+        data.name,
         data.password,
         data.city,
         data.address,
