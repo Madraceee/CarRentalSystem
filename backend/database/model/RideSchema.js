40350bbd8695666bbd94103381d55d0b6d7b1153
madraceee <raajanitheesh@gmail.com>
2023-05-05 05:12:37 +0530

    Ride Schema Added

---
+++ backend/database/model/RideSchema.js
@@ -0,0 +1,25 @@
+class RideSchema {
+    constructor(rideID,lenderID,renterID, listingID, duration, distance,rideStatus) {
+      this.rideID = rideID;
+      this.lenderID = lenderID;
+      this.renterID = renterID;
+      this.listingID = listingID;
+      this.duration = duration;
+      this.distance = distance;
+      this.rideStatus = rideStatus;
+    }
+  
+    static create(data) {
+      return new RideSchema(
+        data.rideID,
+        data.lenderID,
+        data.renterID,
+        data.listingID,
+        data.duration,
+        data.distance,
+        data.rideStatus
+      );
+    }
+  }
+  
+  export default RideSchema;
