d95ca2fc539101d8786f3ee6b758e2c64f8f4851
poolsiderobot <roboticpurple44@gmail.com>
2023-05-04 20:02:59 +0530

    insertProfileRoute added

--- backend/controllers/router.js
+++ backend/controllers/router.js
@@ -4,6 +4,7 @@ import auth from "./private/auth.js";
 import displayRoute from "./private/showProfileRoute.js";
 import insertCarDetailsRoute from "./private/insertCarDetailsRoute.js"
 import showCarDetailsRoute from "./private/showCarDetailsRoute.js"
+import insertProfileRoute from "./private/insertProfileRoute.js"
 
 
 const routes = {
@@ -11,7 +12,8 @@ const routes = {
     "showProfile": async (data,res) => displayRoute(data,res),
     "notFound" :  (data,res)=>notFound(res),
     "insertCar": async (data,res)=>insertCarDetailsRoute(data,res),
-    "displayCar": async(data,res)=> showCarDetailsRoute(data,res)
+    "displayCar": async(data,res)=> showCarDetailsRoute(data,res),
+    "insertProfile": async(data,res)=> insertProfileRoute(data,res)
 }
 
 export default routes;
