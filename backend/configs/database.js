50ee9bba507de949ee5cef7aad06d4a5da1f9636
poolsiderobot <roboticpurple44@gmail.com>
2023-05-05 14:58:37 +0530

    .

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,6 +1,6 @@
 const HOST = "sql12.freemysqlhosting.net"
 const USERNAME = "sql12615857";
 const PASSWORD = "";
-const DATABASE = "sql12615857"; 
+const DATABASE = "sql12615857";  
 
 export { HOST,USERNAME,PASSWORD,DATABASE }
