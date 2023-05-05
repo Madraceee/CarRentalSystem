46b94b85c330f69d683ccafc2836a5403fce61f1
poolsiderobot <roboticpurple44@gmail.com>
2023-05-05 14:40:06 +0530

    Added RideSchema and routes

---
+++ backend/controllers/private/insertRideRoute.js
@@ -0,0 +1,31 @@
+import rideSchema from "../database/model/RideSchema.js";
+import Ride from "../../database/repository/ride.js";
+
+
+async function insertRideRoute(data, res) {
+  // Parse request data and create profile object
+      
+    try {
+
+        const jsonData = JSON.parse(data.data);
+        const rideObj = rideSchema.create(jsonData);
+        
+              const payload =    await Ride.insertRide(rideObj);
+
+
+              res.setHeader("Content-Type", "application/json");
+              res.setHeader("Access-Control-Allow-Origin", "*");
+
+
+              // Send success message
+              res.writeHead(200);
+              res.write('Ride successfully inserted!');
+              res.end("\n");
+            } catch (error) {
+              console.error(error);
+              console.log("Insert Ride Error:" + error);
+            }
+          };
+          
+
+ export default insertRideRoute
--- backend/controllers/router.js
+++ backend/controllers/router.js
@@ -3,19 +3,22 @@ import notFound from "./notFound.js";
 import auth from "./private/auth.js";
 import displayRoute from "./private/showProfileRoute.js";
 import insertCarDetailsRoute from "./private/insertCarDetailsRoute.js"
-import showCarDetailsRoute from "./public/showCarDetailsRoute.js"
-import insertProfileRoute from "./public/insertProfileRoute.js"
-import getUserCars from "./private/showUserCars.js"
+import showCarDetailsRoute from "./private/showCarDetailsRoute.js"
+import insertProfileRoute from "./private/insertProfileRoute.js"
+import insertRideRoute from "./private/insertRideRoute.js";
+import showRideRoute from "./private/showRideRoute.js"
+import res from "express/lib/response.js";
 
 
 const routes = {
     "login" : async (data,res)=>loginRoute(data,res),
     "showProfile": async (data,res) => displayRoute(data,res),
+    "notFound" :  (data,res)=>notFound(res),
     "insertCar": async (data,res)=>insertCarDetailsRoute(data,res),
     "displayCar": async(data,res)=> showCarDetailsRoute(data,res),
     "insertProfile": async(data,res)=> insertProfileRoute(data,res),
-    "getUserCars": async (data,res) => getUserCars(data,res),
-    "notFound" :  (data,res)=>notFound(res)
-}
+    "insertRide" : async(data,res) => insertRideRoute(data,res),
+    "showRide" : async(data,res) => showRideRoute(data,res)
+    }
 
 export default routes;
